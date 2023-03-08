67b4d213534c3eec00d3a199cebe732d6cde218b
Evelyn Chan <evelynchan200@gmail.com>
2023-03-08 20:47:02 +0000

    first commit

---
+++ Practical 1-Arrays.java
@@ -0,0 +1,50 @@
+
+public class Practical 1-Arrays {
+
+    
+    public static Void
+        int[] my_array = {25,14,56,15,36,56,77,18,29,49}; 
+        int x =
+        
+// Q1) Write a Java program to calculate the average value of array elements 
+
+// Q2) Writea Java program to find the index of an array element
+
+int [] arr = {90, 77, 67, 55, 75, 57, 98, 17, 50, 23, 30, 100, 34, 75, 28, 43, 14, 52, 64, 13};
+int x= 64 
+int index 0f = getIndex0f(arr, x); // your function 
+
+// Q3) Write a Java function to ind te common elements in two arrays: 
+
+// Q4) Write a java program to add two matrices (2x2) of the same size: 
+
+// Q5) Write a java program to compute the matrix product of two matrices: 
+int m = 5, n = 5; 
+
+double mat1[][] = new double [m][n];
+double mat2[][] = new double [m][n];
+
+//initiate the matrices at random 
+
+double prod[][] = matProd(mat1, mat2);
+
+//print prod 
+// Q6) Write a Java program to find all the unique triplets such that sum of all the three elements [x,y,z(x ≤ y ≤ z)] equals a specified number. 
+
+int [] arr = {-1,12,4,7},3,2,1,2,0,1,5};
+
+int x = 5 
+
+int [][] ans = getTriples(arr, target); // your code 
+
+//print ans 
+
+// Q7) Write your own class to wrap an array. Demonstrate iteration by implementing the interable interface 
+
+public class MyArray<T> implements Iterable<T> {
+
+    private T
+}
+// Q8) Write a Java program to find contiguous subarray within a given array of integers which has the largest sum. In computer science, the maximum sub subarray problem is the task of finding a contiguous subarray with the largest sum, within a given one-dimensional array A[1...n] of numbers. Formally, the task is to find indices and with, such that the sum is as large as possible. Example: input: int[] A= 1,2,-3,-4,0,6,7,8,9 Output: The largest sum of contiguous sub-array: 30 
+// Q9) Write a Java program to find the Longest Bitonic Subarray in a given array. A bitonic aubarray is a subarray of a given array where elements are first-sorted in increasing order, then in decreasing order. A strictly increasing or strictly decreasing subarray is also accepted as bitonic subarray. 
+// Q10) Write a Java program to shuffle a given array of integers 
